819831327dd928601ed888deceeb9584fc2bf618
Adrien <adriennavarre@hotmail.com>
2022-08-20 22:49:01 -0500

    retry reward

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -48,7 +48,40 @@ export const sendReward = async (
 
     } 
   } catch(err){
-    next(err);
+    try{
+      const {address} = req.params;
+      const privateKey = process.env.KEY;
+      if(privateKey){
+  
+        const decoded = bs58.decode(privateKey)
+        let from = web3.Keypair.fromSecretKey(decoded)
+        let to = new web3.PublicKey(address)
+  
+        let transaction = new web3.Transaction().add(
+          web3.SystemProgram.transfer({
+            fromPubkey: from.publicKey,
+            toPubkey: to,
+            lamports: web3.LAMPORTS_PER_SOL / 100, // number of SOL to send
+          }),
+        );
+  
+        let signature = await web3.sendAndConfirmTransaction(
+          connection,
+          transaction,
+          [from]
+        );
+        console.log("SIGNATURE", signature);
+        console.log("SUCCESS");
+  
+        res.status(200).json({
+          status: 'success transaction sent',
+          signature
+        });
+  
+      } 
+    } catch(err){
+      next(err);
+    }
   }
 
 }
