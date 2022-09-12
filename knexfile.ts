dd1bce3b6e5d9008e3af2a6f6e9a979bb862ff77
Adrien <adriennavarre@hotmail.com>
2022-09-12 00:11:07 -0500

    force try to update

--- knexfile.ts
+++ knexfile.ts
@@ -1,29 +1,18 @@
 require('dotenv').config();
 
 const config = {
-    development: {
-      client: 'postgresql',
-      connection: {
-        host: process.env.PGHOST,
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE
-      },
-      migrations: {
-          tableName: 'knex_migrations',
-          directory: `${ __dirname }/db/migrations`
-      }
-    },
     production: {
       client: 'postgresql',
-      connection: process.env.DATABASE_URL,
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      },
       pool: {
         min: 2,
         max: 10
       },      
-      ssl: {
-        rejectUnauthorized: false
-      },
       migrations: {
         tableName: 'knex_migrations',
         directory: `${ __dirname }/db/migrations`
